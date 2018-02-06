a9d3cb8626f2cdc2ba017370e61337581d27a155
MKMichalski <mateuszmichalskiK@gmail.com>
2018-02-06 23:18:46 +0100

    generateNewWorld method implemented

--- src/main/java/gameOfLife/Run.java
+++ src/main/java/gameOfLife/Run.java
@@ -6,12 +6,17 @@ public class Run {
     public static void main(String[] args) {
 
 
-        World world = new World(50);
-        world.createRandomWorld();
-        world.displayWorld();
-        world.generateNewWorld();
+        World world = new World(5);
+        world.createRandomWorld(10);
 
+       int timer = 10;
 
+       while (timer > 0) {
+           world.displayWorld();
+           world.generateNewWorld();
+           world.displayWorld();
+            --timer;
+       }
 
     }
 }
--- src/main/java/gameOfLife/World.java
+++ src/main/java/gameOfLife/World.java
@@ -3,52 +3,140 @@ package gameOfLife;
 
 import java.util.Random;
 
-public class World{
-
+public class World {
+    private LiveCell livecell;
+    private DeadCell deadcell;
     private int size;
-    private World world [][];
-    public World (){}
+    private World world[][];
 
-    public World(int size ) {
-        this.size = size;
-        world = new World [size][size];
+    public World() {
     }
 
+    public World(int size) {
+        this.size = size;
+        world = new World[size][size];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 
     public void displayWorld() {
-        for (int i = 0; i < world.length; i++) {
-            for (int j = 0; j < world.length; j++) {
+        for (int i = 1; i < world.length; i++) {
+            for (int j = 1; j < world.length; j++) {
                 System.out.print(world[i][j]);
             }
             System.out.println();
         }
+        System.out.println("------------------------------------------------------");
         System.out.println();
     }
 
-    public void createRandomWorld(){
-        LiveCell livecell = new LiveCell();
-        DeadCell deadcell = new DeadCell();
+    /**
+     * This method create initial "world"
+     *
+     * @param density should be in range 0-10, density defines density of population.
+     */
+    public void createRandomWorld(int density) {
+
+
+        livecell = new LiveCell();
+        deadcell = new DeadCell();
         Random r = new Random();
 
         for (int i = 0; i < world.length; i++) {
             for (int j = 0; j < world.length; j++) {
-                int x = r.nextInt(2);
-                if(x == 1){
-                    world[i][j] =  livecell ;
+                int x = r.nextInt(12 - density);
+                if (x == 1) {
+                    world[i][j] = livecell;
 
-                }else{
+                } else {
                     world[i][j] = deadcell;
                 }
             }
         }
     }
 
+    public void generateNewWorld() throws ArrayIndexOutOfBoundsException {
 
-    public void generateNewWorld() {
+        for (int i = 1; i < world.length - 1; i++) {
+            for (int j = 1; j < world.length - 1; j++) {
+                if (world[i][j] == livecell) {
+                    int counterLive = 0;
+                    if (world[i - 1][j - 1] == livecell) {
+                        counterLive++;
+                    }
+                    if (world[i - 1][j] == livecell) {
+                        counterLive++;
+                    }
+                    if (world[i - 1][j + 1] == livecell) {
+                        counterLive++;
+                    }
+                    if (world[i][j - 1] == livecell) {
+                        counterLive++;
+                    }
+                    if (world[i][j + 1] == livecell) {
+                        counterLive++;
+                    }
+                    if (world[i + 1][j - 1] == livecell) {
+                        counterLive++;
+                    }
+                    if (world[i + 1][j] == livecell) {
+                        counterLive++;
+                    }
+                    if (world[i + 1][j + 1] == livecell) {
+                        counterLive++;
+                    }
+
+                    if (counterLive < 2 || counterLive > 3) {
+                        world[i][j] = deadcell;
+                    } else {
+                        world[i][j] = livecell;
+                    }
+
+                }
+                if (world[i][j] == deadcell) {
+                    int counterLive1 = 0;
+                    if (world[i - 1][j - 1] == livecell) {
+                        counterLive1++;
+                    }
+                    if (world[i - 1][j] == livecell) {
+                        counterLive1++;
+                    }
+                    if (world[i - 1][j + 1] == livecell) {
+                        counterLive1++;
+                    }
+                    if (world[i][j - 1] == livecell) {
+                        counterLive1++;
+                    }
+                    if (world[i][j + 1] == livecell) {
+                        counterLive1++;
+                    }
+                    if (world[i + 1][j - 1] == livecell) {
+                        counterLive1++;
+                    }
+                    if (world[i + 1][j] == livecell) {
+                        counterLive1++;
+                    }
+                    if (world[i + 1][j + 1] == livecell) {
+                        counterLive1++;
+                    }
+                    if (counterLive1 == 3) {
+                        world[i][j] = livecell;
+                    } else {
+                        world[i][j] = deadcell;
+                    }
 
 
-
-
-
+                }
+            }
+        }
     }
+
+
 }
+
